3a49427eea530d286d4c652b2c803d6f0ff3bc32
Reece Harris <reece.harris98@protonmail.com>
2025-04-27 22:59:16 +0100

    fix: update CPU data handling in Svelte component; adjust server emission interval for current load

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 
     const HISTORY_LENGTH = 50;
 
-    let cpus: number[][] = $state([]);
+    let currentLoad: number[][] = $state([]);
 
     onMount(() => {
         const socket = io("192.168.1.102:3000", {
@@ -19,10 +19,9 @@
             console.log("Message from server:", data);
         });
 
-        socket.on("cpu", (data) => {
+        socket.on("currentLoad", (data) => {
             console.log("CPU data:", data);
-            /* cpus.push(data.cpus.map((cpu) => cpu.load));
-            cpus = cpus.slice(-HISTORY_LENGTH); */
+            currentLoad.push(data.cpus.map((cpu) => cpu.load));
         });
     });
 
@@ -37,24 +36,42 @@
     use([LineChart, GridComponent, CanvasRenderer, TitleComponent]);
 </script>
 
-<!-- <div class="h-100 w-200">
-    <Chart
-        {init}
-        options={{
-            xAxis: {
-                type: "category",
-                boundaryGap: false,
-                data: Array.from({ length: HISTORY_LENGTH }, (_, i) => ""),
-            },
-            yAxis: {
-                type: "value",
-            },
-            series: Array.from({ length: cpus.length ? cpus[0].length : 0 }, (_, i) => ({
-                data: cpus.map((cpu) => cpu[i]),
-                type: "line",
-                areaStyle: {},
-            })),
-        }}
-    />
+<div class="flex flex-col">
+    <div class="h-100 w-200">
+        <Chart
+            {init}
+            options={{
+                xAxis: {
+                    type: "category",
+                    boundaryGap: false,
+                    data: Array.from({ length: HISTORY_LENGTH }, (_, i) => ""),
+                },
+                yAxis: {
+                    type: "value",
+                },
+                series: Array.from({ length: currentLoad.length ? currentLoad[0].length : 0 }, (_, i) => ({
+                    data: currentLoad.map((cpu) => cpu[i]).slice(-50),
+                    type: "line",
+                    areaStyle: {},
+                    symbol: "none", // Removes the dots/markers from the line
+                })),
+            }}
+        />
+    </div>
+
+    <div class="flex flex-col">
+        <div class="flex flex-col">
+            {#each {length: currentLoad.length ? currentLoad[0].length : 0}, i}
+                <div class="flex gap-4">
+                    <h2>CPU {i}</h2>
+                    <span>|</span>
+                    <span>cur {currentLoad[currentLoad.length-1][i].toFixed(2)}%</span>
+                    <span>avg {(currentLoad.reduce((acc, cpu) => acc + cpu[i], 0) / currentLoad.length).toFixed(2)}%</span>
+                    <span>min {Math.min(...currentLoad.map(cpu => cpu[i])).toFixed(2)}%</span>
+                    <span>max {Math.max(...currentLoad.map(cpu => cpu[i])).toFixed(2)}%</span>
+                </div>
+            {/each}
+        </div>
+
+    </div>
 </div>
- -->

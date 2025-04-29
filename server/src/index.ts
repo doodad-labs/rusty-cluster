79fabcb75110ea27d213679e04a7d8e4ee5d644d
Reece Harris <reece@codegalaxy.co.uk>
2025-04-29 15:02:45 +0100

    feat: implement socket authentication using a key from a file and handle file creation

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,25 @@ import { Server } from 'socket.io';
 import fs from 'fs/promises';
 import crypto from 'crypto';
 
+const filePath = 'key.rusty';
+const key = checkAndProcessFile(filePath).catch(err => console.error('Error:', err));
+
 const server = createServer();
 const io = new Server(server);
 
+io.use((socket, next) => {
+    const token = socket.handshake.auth.token; // Get the token from the handshake
+
+    if (token === key) { // Compare with the key
+        next(); // Token is valid, proceed to the connection
+    } else {
+        next(new Error('Authentication error')); // Optionally, you can send an error message
+        socket.disconnect(true); // Token is invalid, disconnect the socket
+    }
+})
+
 io.on('connection', async (socket) => {
     socket.join('clusterInfo');
-
     const osInfo = await sysinfo.osInfo(); // Get OS information
     socket.emit('osInfo', osInfo)
 });
@@ -54,25 +67,23 @@ server.listen(3000, () => {
     console.log('server running at http://localhost:3000');
 });
 
-async function checkAndProcessFile(filePath: string): Promise<void> {
+async function checkAndProcessFile(filePath: string): Promise<string> {
     try {
         // Check if file exists
         await fs.access(filePath);
 
         // File exists - read and print it
         const content = await fs.readFile(filePath, 'utf-8');
-        console.log('File content:');
-        console.log(content);
+        return content;
     } catch (error: any) {
         if (error.code === 'ENOENT') {
             // File doesn't exist - create it with random string
             const randomString = crypto.randomBytes(32).toString('hex'); // 256 bits = 32 bytes
-
-            console.log(`File doesn't exist. Creating new file with random string:`);
-            console.log(randomString);
-
             await fs.writeFile(filePath, randomString, 'utf-8');
-            console.log('File created successfully.');
+
+            console.log(`Your cluster key is: "${randomString}", saved to "${filePath}"`);
+
+            return randomString;
         } else {
             // Other error occurred
             throw error;
@@ -94,6 +105,3 @@ function GracefulShutdown() {
 
 process.on('SIGTERM', GracefulShutdown);
 process.on('SIGINT', GracefulShutdown);
-
-const filePath = 'key.rusty';
-checkAndProcessFile(filePath).catch(err => console.error('Error:', err));
